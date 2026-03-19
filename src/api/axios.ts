fbf7cf4a438bd98d83a5e203796e4b40a68da262
Sewak Kaur <bhumikrishnani211@gmail.com>
2026-03-19 11:45:56 +0530

    Update axios.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,23 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://study-stream-api.onrender.com/api",
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json"
-  }
-});
-
-// interceptor
-api.interceptors.request.use((config) => {
-
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-
+  withCredentials: true
 });
 
 export default api;
