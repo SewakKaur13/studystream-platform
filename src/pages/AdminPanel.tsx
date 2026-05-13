2baadc66afc498d777b474f2471e77bdc19343d1
Sewak Kaur <bhumikrishnani211@gmail.com>
2026-05-13 19:50:53 +0530

    Update AdminPanel.tsx

--- src/pages/AdminPanel.tsx
+++ src/pages/AdminPanel.tsx
@@ -590,7 +590,7 @@ const AdminPanel = () => {
                     </Badge>
                   </div>
 
-                  <div className="flex gap-2">
+                  <div className="flex flex-wrap gap-2">
                     <Button
                       variant="outline"
                       size="sm"
